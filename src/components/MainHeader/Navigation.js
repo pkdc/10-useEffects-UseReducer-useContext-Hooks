ef7552df100f9c117cda72fbedf6377153352283
pkdc <pkdchoy@gmail.com>
2021-08-14 14:20:24 +0100

    use the useContext hook instead of the Consumer.

--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -1,57 +1,62 @@
-import React from 'react';
+import React, {useContext} from 'react';
 
 import classes from './Navigation.module.css';
 import AuthContext from '../../store/auth-context';
 
 const Navigation = (props) => {
-  // return (
-  //   <nav className={classes.nav}>
-  //     <ul>
-  //       {props.isLoggedIn && (
-  //         <li>
-  //           <a href="/">Users</a>
-  //         </li>
-  //       )}
-  //       {props.isLoggedIn && (
-  //         <li>
-  //           <a href="/">Admin</a>
-  //         </li>
-  //       )}
-  //       {props.isLoggedIn && (
-  //         <li>
-  //           <button onClick={props.onLogout}>Logout</button>
-  //         </li>
-  //       )}
-  //     </ul>
-  //   </nav>
-  // );
-return (
-  <AuthContext.Consumer>
-    {(ctx) => {
-      return (
-        <nav className={classes.nav}>
-          <ul>
-            {ctx.isLoggedIn && (
-              <li>
-                <a href="/">Users</a>
-              </li>
-            )}
-            {ctx.isLoggedIn && (
-              <li>
-                <a href="/">Admin</a>
-              </li>
-            )}
-            {ctx.isLoggedIn && (
-              <li>
-                <button onClick={props.onLogout}>Logout</button>
-              </li>
-            )}
-          </ul>
-        </nav>
-      );
-    }}
-  </AuthContext.Consumer>
-);
+  // using useContext Hook
+  const ctx = useContext(AuthContext);
+  
+  return (
+    <nav className={classes.nav}>
+      <ul>
+        {ctx.isLoggedIn && (
+          <li>
+            <a href="/">Users</a>
+          </li>
+        )}
+        {ctx.isLoggedIn && (
+          <li>
+            <a href="/">Admin</a>
+          </li>
+        )}
+        {ctx.isLoggedIn && (
+          <li>
+            <button onClick={props.onLogout}>Logout</button>
+          </li>
+        )}
+      </ul>
+    </nav>
+  );
+
+  // using Context Consumer
+// return (
+//   <AuthContext.Consumer>
+//     {(ctx) => {
+//       return (
+//         <nav className={classes.nav}>
+//           <ul>
+//             {ctx.isLoggedIn && (
+//               <li>
+//                 <a href="/">Users</a>
+//               </li>
+//             )}
+//             {ctx.isLoggedIn && (
+//               <li>
+//                 <a href="/">Admin</a>
+//               </li>
+//             )}
+//             {ctx.isLoggedIn && (
+//               <li>
+//                 <button onClick={props.onLogout}>Logout</button>
+//               </li>
+//             )}
+//           </ul>
+//         </nav>
+//       );
+//     }}
+//   </AuthContext.Consumer>
+// );
 };
 
 export default Navigation;
